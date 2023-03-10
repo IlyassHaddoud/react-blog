fe805a8e921b25c778c403df88446fda9c86955f
ilyass <79806692+illyasshaddoud@users.noreply.github.com>
2023-03-10 23:04:17 +0100

    first commit

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo">Blog</div>
+      <div className="list">
+        <li>Home</li>
+        <li>Add blog</li>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
